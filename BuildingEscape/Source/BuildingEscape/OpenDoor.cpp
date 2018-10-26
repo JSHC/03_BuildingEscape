55d310a8b6b0c5fc73ccde45ef7ea9886e0f8801
Johan Hansson <johan.hansson.carlberg@gmail.com>
2018-10-26 12:51:00 +0200

    Added OpenDoor Class and added a door and frame to the level

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Johan Hansson Carlberg 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	//Find Actor
+	AActor* Owner = GetOwner();
+	//Create rotator
+	FRotator NewRotation = FRotator(0.0f, -70.0f, 0.0f);
+	//Set rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
