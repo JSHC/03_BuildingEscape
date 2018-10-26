887e551ea32acb03a65231002d56012199e64982
Johan Hansson <johan.hansson.carlberg@gmail.com>
2018-10-26 20:07:18 +0200

    Added functionality for getting the player viewpoint location and rotation and logging it to the engine

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Johan Hansson Carlberg 2018
 
 #include "Grabber.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -31,5 +34,16 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	// Get player viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	//Log out to test
+	UE_LOG(LogTemp, Warning, TEXT("Position: %s | Rotation: %s"), *PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
+	// Ray-cast to player reach
+	// check if player is looking at a grabbable actor within distance
 }
 
